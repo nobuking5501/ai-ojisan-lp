478d2f227b352f42a9b72d957b03c8d470680a3d
nobuking5501 <nobuking5501@gmail.com>
2025-05-06 20:20:58 +0900

    Fix Vercel deployment settings

--- next.config.js
+++ next.config.js
@@ -4,6 +4,9 @@ const nextConfig = {
   images: {
     domains: ['placehold.jp', 'placehold.co'],
   },
+  swcMinify: true,
+  poweredByHeader: false,
+  trailingSlash: false,
 }
 
 module.exports = nextConfig
